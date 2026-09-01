0b250882c2588d5039da8bc5dd45e29389723b54
thelight <gokulka2006@gmail.com>
2026-09-01 15:49:23 +0530

    change

---
+++ frontend/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app.jsx";
+import "./styles.css";
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Target container #root not found in DOM.");
+}
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
